8b8c4fbeed5fa134cb16e7baa6fd4d223c9a6294
amypepper <amycarlsonpepper@gmail.com>
2020-10-13 08:00:48 -0700

    add api auth andcors

--- src/app.js
+++ src/app.js
@@ -3,14 +3,30 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
-const { NODE_ENV } = require("./config");
+const { NODE_ENV, CLIENT_ORIGIN } = require("./config");
 
 const app = express();
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN,
+  })
+);
+
+///////////////////// API KEY VALIDATION /////////////////////
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    console.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+  next();
+});
 
 /////////////////////  HOME ENDPOINT /////////////////////
 app.get("/api/*", (req, res) => {
