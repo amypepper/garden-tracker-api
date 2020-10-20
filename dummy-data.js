0cfa9d083b30a5fd66ae40aac21c3195c8b460f5
amypepper <amycarlsonpepper@gmail.com>
2020-10-20 07:13:50 -0700

    create dummy date file

---
+++ dummy-data.js
@@ -0,0 +1,17 @@
+const users = [
+  {
+    id: "gawe987yaehgpiubrfta",
+    email: "iamawesome@me.com",
+    password: "best password ever",
+  },
+  {
+    id: "piu9087uyh0on3497a",
+    email: "thisisme@gmail.com",
+    password: "super secret password",
+  },
+  {
+    id: "poebfd739bng437ey",
+    email: "whatsupbuttercup@gmail.com",
+    password: "stapler weight puff baby",
+  },
+];
--- src/app.js
+++ src/app.js
@@ -11,24 +11,6 @@ const { v4: uuid } = require("uuid");
 
 ///////////////////// DUMMY DATA /////////////////////
 
-const users = [
-  {
-    id: "gawe987yaehgpiubrfta",
-    email: "iamawesome@me.com",
-    password: "best password ever",
-  },
-  {
-    id: "piu9087uyh0on3497a",
-    email: "thisisme@gmail.com",
-    password: "super secret password",
-  },
-  {
-    id: "poebfd739bng437ey",
-    email: "whatsupbuttercup@gmail.com",
-    password: "stapler weight puff baby",
-  },
-];
-
 app.use(express.json());
 app.use(morgan(morganOption));
 app.use(helmet());
