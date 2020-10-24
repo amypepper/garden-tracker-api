4b28909b95410fb2a42d4ac48dab7c7282c26f1d
amypepper <amycarlsonpepper@gmail.com>
2020-10-24 16:29:04 -0700

    prepare for deploy

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  CLIENT_ORIGIN: "https://garden-tracker-client.vercel.app/",
+  CLIENT_ORIGIN:
+    "https://garden-tracker-client.vercel.app/" || "http://localhost:3000",
   DATABASE_URL:
     process.env.DATABASE_URL || "postgresql://postgres@localhost/gardentracker",
 };
